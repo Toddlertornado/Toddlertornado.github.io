de2a9e71db9dd0534a279420391fff7d33c89d1b
Shreya Ghosh <65737787+Toddlertornado@users.noreply.github.com>
2020-06-04 21:39:02 -0700

    Create SnowMan.js

---
+++ SnowMan.js
@@ -0,0 +1,20 @@
+//Snowman
+background(255, 255, 255);
+
+// COOKIE
+noStroke();
+fill(186, 179, 101);
+ellipse(200, 200, 300, 300);
+fill(87, 69, 31);
+ellipse(88,181,30,30);
+ellipse(289,139,30,30);
+ellipse(146,266,30,30);
+ellipse(206,182,30,30);
+ellipse(255,283,30,30);
+ellipse(143,111,30,30);
+draw = function() {
+  // take a bite out of the COOKIE!
+fill(255, 255, 255);
+ellipse(mouseX, mouseY, 100, 100); 
+
+};
